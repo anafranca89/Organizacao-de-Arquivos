d1818ee5fcd57cf52c0b803e12f0c6900f2d15bc
Ana Julia França <158233360+anafranca89@users.noreply.github.com>
2026-03-29 18:54:52 -0300

    Funções auxiliares de escrita e leitura de registros/arquivos

--- registros.c
+++ registros.c
@@ -1,37 +1,91 @@
 #include <stdio.h>
-#include <stdlib.h>
-/*Nomes:
-Ana Julia Lopez Franqueira de França - 16838230
-Leonardo Biondo Bertho - 16881900            -
-*/
+#include "registros.h"
 
-FILE *binario = NULL;
+/*Funções para o ARQUIVO BINÁRIO DE SAÍDA. Retornam o ponteiro de arquivo  fopen!*/
+FILE * ler_binario(char *arqbin){
+    FILE *file = fopen(arqbin, "rb");
+    if(file == NULL){
+        return NULL;
+    }
+    return file;
+}
 
-/*A representação é de 17 bytes. Como saber se tem essa qts de bytes....
-pela ordem que foi chamado as variáveis*/
-typedef struct regcabecalho {
-    char status;
-    int topo;
-    int proxRRN;
-    int nroEstacoes;
-    int nroParesEstacoes;
-} cabecalho;
+FILE * escrever_binario(char *arqbin){
+    FILE *file = fopen(arqbin, "r+b");
+    if(file == NULL){
+        return NULL;
+    }
+    return file;
+
+}
 
 
+/*Dado um arquivo binário, a função precisa ler o registro de cabeçalho */
+cabecalho ler_cabecalho(char * arqbin){
+    cabecalho regcab;
+    FILE *bin = ler_binario(arqbin);
+    fread(&regcab.status, sizeof(char), 1, bin);
+    fread(&regcab.topo, sizeof(int), 1, bin);
+    fread(&regcab.proxRRN, sizeof(int), 1, bin);
+    fread(&regcab.nroEstacoes, sizeof(int), 1, bin);
+    fread(&regcab.nroParesEstacoes, sizeof(int), 1, bin);
+    fclose(bin);
+    return regcab;
+}
 
-typedef struct regdados {
-    char removido;
-    int codEstacao;
-    int codLinha;
-    int codProxEstacao;
-    int distProxEstacao;
-    int codLinhaIntegra;
-    int codEstIntegra;
-    int tamNomeEstacao;
-    char  nomeEstacao[tamNomeEstacao];
-    int tamNomeLinha;
-    char nomeLinha[tamNomeLinha];
-} dados;
+/* Dado o arquivo binário, precisa atualizar os valores no cabeçalho*/
+void escreve_cabecalho(char *arqbin, cabecalho regcab){
+    FILE *bin = escrever_binario(arqbin);
+    fseek(bin, 0, SEEK_SET);
+    fwrite(&regcab.status, sizeof(char), 1, bin);
+    fwrite(&regcab.topo, sizeof(int), 1, bin);
+    fwrite(&regcab.proxRRN, sizeof(int), 1, bin);
+    fwrite(&regcab.nroEstacoes, sizeof(int), 1, bin);
+    fwrite(&regcab.nroParesEstacoes, sizeof(int), 1, bin);
+    fclose(bin);
+}
+
+/*Leitura e Escrita de Registros. 
+Dado o arquivo binário,  e o byteoffset do registro escolhido, retorna ou escreve as informções naquele registro*/
+dados ler_regdados(char *arqbin, int byteoffset){
+    dados registro;
+    FILE *bin = ler_binario(arqbin);
+    fseek(bin, byteoffset, SEEK_SET);
+    fread(&registro.removido, sizeof(char), 1, bin);
+    fread(&registro.proximo, sizeof(int), 1, bin);
+    fread(&registro.codEstacao, sizeof(int), 1, bin);
+    fread(&registro.codLinha, sizeof(int), 1, bin);
+    fread(&registro.codProxEstacao, sizeof(int), 1, bin);
+    fread(&registro.distProxEstacao, sizeof(int), 1, bin);
+    fread(&registro.codLinhaIntegra, sizeof(int), 1, bin);
+    fread(&registro.codEstIntegra, sizeof(int), 1, bin);
+    fread(&registro.tamNomeEstacao, sizeof(int), 1, bin);
+    fread(registro.nomeEstacao, sizeof(char), registro.tamNomeEstacao, bin);
+    fread(&registro.tamNomeLinha, sizeof(int), 1, bin);
+    fread(registro.nomeLinha, sizeof(char), registro.tamNomeLinha, bin);
+    fclose(bin);
+
+    return registro;
+}
+
+void escreve_regdados(char *arqbin, dados registro, int byteoffset){
+
+    FILE *bin = escrever_binario(arqbin);
+    fseek(bin, byteoffset, SEEK_SET);
+    fwrite(&registro.removido, sizeof(char), 1, bin);
+    fwrite(&registro.proximo, sizeof(int), 1, bin);
+    fwrite(&registro.codEstacao, sizeof(int), 1, bin);
+    fwrite(&registro.codLinha, sizeof(int), 1, bin);
+    fwrite(&registro.codProxEstacao, sizeof(int), 1, bin);
+    fwrite(&registro.distProxEstacao, sizeof(int), 1, bin);
+    fwrite(&registro.codLinhaIntegra, sizeof(int), 1, bin);
+    fwrite(&registro.codEstIntegra, sizeof(int), 1, bin);
+    fwrite(&registro.tamNomeEstacao, sizeof(int), 1, bin);
+    fwrite(registro.nomeEstacao, sizeof(char), registro.tamNomeEstacao, bin);
+    fwrite(&registro.tamNomeLinha, sizeof(int), 1, bin);
+    fwrite(registro.nomeLinha, sizeof(char), registro.tamNomeLinha, bin);
+    fclose(bin);
+}
 
 
 
