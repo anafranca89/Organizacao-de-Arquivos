de2b213d8a6b746a43c484b9ca7dc0e52590061e
bertholeo <leobbertho42@gmail.com>
2026-04-04 13:02:29 -0300

    Update registros.h

--- registros.h
+++ registros.h
@@ -4,40 +4,6 @@
 #define TAM_REG 80
 #define TAM_CABECALHO 17
 
-
-/*A representação é de 17 bytes. Como saber se tem essa qts de bytes....
-pela ordem que foi chamado as variáveis*/
-typedef struct regcabecalho {
-    char status;
-    int topo;
-    int proxRRN;
-    int nroEstacoes;
-    int nroParesEstacoes;
-}cabecalho;
-
-
-/*Valores nulos são representados por -1 para campos de tam.fixo.
-Valores nulos nos Campos de tam.variável tem 0 no indicador de tamanho
-80 bytes!!!
-*/
-
-typedef struct regdados{
-    char removido;
-    int proximo;
-    int codEstacao;
-    int codLinha;
-    int codProxEstacao;
-    int distProxEstacao;
-    int codLinhaIntegra;
-    int codEstIntegra;
-    int tamNomeEstacao;
-    char  nomeEstacao[tamNomeEstacao];
-    int tamNomeLinha;
-    char nomeLinha[tamNomeLinha];
-} dados;
-
-
-
 FILE* ler_binario(char *arqbin);
 FILE * escrever_binario(char *arqbin);
 
