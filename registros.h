f9503df481a1b17649eba623d0ad4f232f91e0de
bertholeo <leobbertho42@gmail.com>
2026-04-04 13:10:48 -0300

    Update registros.h

--- registros.h
+++ registros.h
@@ -7,10 +7,47 @@
 FILE* ler_binario(char *arqbin);
 FILE * escrever_binario(char *arqbin);
 
-cabecalho ler_cabecalho(char *arqbin);
-void escreve_cabecalho(char *arqbin, cabecalho regcab);
-dados ler_regdados(char *arqbin, int byteoffset);
-void escreve_regdados(char *arqbin, dados registro, int byteoffset);
+void ler_cabecalho(char *arqbin,
+                   char *status,
+                   int *topo,
+                   int *proxRRN,
+                   int *nroEstacoes,
+                   int *nroParesEstacoes);
+
+void escreve_cabecalho(char *arqbin,
+                       char status,
+                       int topo,
+                       int proxRRN,
+                       int nroEstacoes,
+                       int nroParesEstacoes);
+
+void ler_regdados(char *arqbin, int byteoffset,
+                  char *removido,
+                  int *proximo,
+                  int *codEstacao,
+                  int *codLinha,
+                  int *codProxEstacao,
+                  int *distProxEstacao,
+                  int *codLinhaIntegra,
+                  int *codEstIntegra,
+                  int *tamNomeEstacao,
+                  char **nomeEstacao,
+                  int *tamNomeLinha,
+                  char **nomeLinha);
+
+void escreve_regdados(char *arqbin, int byteoffset,
+                      char removido,
+                      int proximo,
+                      int codEstacao,
+                      int codLinha,
+                      int codProxEstacao,
+                      int distProxEstacao,
+                      int codLinhaIntegra,
+                      int codEstIntegra,
+                      int tamNomeEstacao,
+                      char *nomeEstacao,
+                      int tamNomeLinha,
+                      char *nomeLinha);
 
 
 
