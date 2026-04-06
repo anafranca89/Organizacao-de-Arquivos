ee6c592e8f6f195f99acecf732434b5158beae48
bertholeo <leobbertho42@gmail.com>
2026-04-06 19:16:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "registros.h"
+#include "funcoes.h"
+#include "fornecidas.h"
 
-int main(){
-  printf("Hello World!")
-  return 0
+int main() {
+    NoHash *tabela[TAM_TABELA];
+    int operacao;
+    char nome_csv[100];
+    char nome_bin[100];
+
+    inicializar_tabela(tabela);
+
+    while (scanf("%d", &operacao) != EOF) {
+
+        if (operacao == 1) {
+            scanf("%s %s", nome_csv, nome_bin);
+
+            FILE *csv = fopen(nome_csv, "r");
+            FILE *bin = fopen(nome_bin, "w+b");
+
+            if (csv == NULL || bin == NULL) {
+                printf("Falha no processamento do arquivo.\n");
+                if (csv != NULL) fclose(csv);
+                if (bin != NULL) fclose(bin);
+                liberar_tabela(tabela);
+                return 0;
+            }
+
+            adicionar_csv_no_binario(csv, bin, tabela);
+
+            fclose(csv);
+            fclose(bin);
+
+            BinarioNaTela(nome_bin);
+        }
+
+        else if (operacao == 2) {
+            scanf("%s", nome_bin);
+
+            FILE *bin = fopen(nome_bin, "rb");
+
+            if (bin == NULL) {
+                printf("Falha no processamento do arquivo.\n");
+                liberar_tabela(tabela);
+                return 0;
+            }
+
+            mostrar_binario_sequencial(bin);
+            fclose(bin);
+        }
+    
+    else if (operacao == 3) {
+        int n, i, j, m;
+
+        scanf("%s", nome_bin);
+
+        FILE *bin = fopen(nome_bin, "rb");
+
+        if (bin == NULL) {
+            printf("Falha no processamento do arquivo.\n");
+            liberar_tabela(tabela);
+            return 0;
+        }
+
+        scanf("%d", &n);
+
+        for (i = 0; i < n; i++) {
+            char nomesCampos[10][50];
+            char valoresCampos[10][200];
+
+            scanf("%d", &m);
+
+            for (j = 0; j < m; j++) {
+                scanf("%s", nomesCampos[j]);
+                ScanQuoteString(valoresCampos[j]);
+            }
+
+            buscar_registros(bin, tabela, m, nomesCampos, valoresCampos);
+    }
+
+    fclose(bin);
+}
+    }
+
+    liberar_tabela(tabela);
+    return 0;
 }
