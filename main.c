14d2c04b281a4aa3148e247e3c7e58e1215c0e3b
anafranca89 <anafranca8933@gmail.com>
2026-06-07 00:44:23 -0300

    Funcao 8 - busca usando arvore b, senao usa busca por registros Falta: modularizar e comentar

--- main.c
+++ main.c
@@ -118,6 +118,11 @@ int main() {
                         liberar_lista_arquivos(lista_arquivos);
                         return 0;
                     }
+
+                    if (!arquivo_ja_processado(lista_arquivos, nome_bin)) {
+                        adicionar_arquivo_processado(&lista_arquivos, nome_bin, bin);
+                        carregar_nomes_no_hash(bin, tabela); // <-- ISSO É VITAL!
+                    }
                     // lê o cabeçalho
                     fseek(bin, 0, SEEK_SET);
                     ler_cabecalho(bin, &cab);
@@ -163,7 +168,9 @@ int main() {
                     return 0;
                 }
 
+                
                 cabecalho cab_insercao;
+                
                 fseek(bin, 0, SEEK_SET);
                 ler_cabecalho(bin, &cab_insercao);
                 if (cab_insercao.status == '0') {
@@ -175,10 +182,15 @@ int main() {
                 cab_insercao.status = '0';
                 fseek(bin, 0, SEEK_SET);
                 escreve_cabecalho(bin, &cab_insercao);
-                
+
                 for (i = 0; i < n; i++) {
-                    ler_e_inserir_registro(bin, tabela);
+                    ler_e_inserir_registro(bin, tabela, &cab_insercao);
                 }
+
+                // volta a colocar o arquivo como funcional
+                cab.status = '1';
+                fseek(bin, 0, SEEK_SET);
+                escreve_cabecalho(bin, &cab);
 
                 fclose(bin);
                 BinarioNaTela(nome_bin);
@@ -235,7 +247,7 @@ int main() {
                 cab.status = '1';
                 fseek(bin, 0, SEEK_SET);
                 escreve_cabecalho(bin, &cab);
-
+                fclose(bin);
                 BinarioNaTela(nome_bin);
                 break;
 
@@ -259,7 +271,93 @@ int main() {
                 break;
             case 8:
 
+                scanf("%s %s", nome_bin, nome_index);
 
+                bin = ler_binario(nome_bin);
+                FILE* index = ler_binario(nome_index);
+                if (bin == NULL || index== NULL) {
+                    printf("Falha no processamento do arquivo.\n");
+                    liberar_tabela(tabela);
+                    liberar_lista_arquivos(lista_arquivos);
+                    return 0;
+                }
+
+                if (!arquivo_ja_processado(lista_arquivos, nome_bin)) {
+                    adicionar_arquivo_processado(&lista_arquivos, nome_bin, NULL);
+                    carregar_nomes_no_hash(bin, tabela);
+                }
+                cab_indice ci;
+                ler_ind_cabecalho(index, &ci);
+
+                scanf("%d", &n);
+
+                for (i = 0; i < n; i++) {
+                    char nomesCampos[8][50];
+                    char valoresCampos[8][200];
+
+                    scanf("%d", &m);
+
+                    for (j = 0; j < m; j++) {
+                        ler_par_campo_valor(nomesCampos[j], valoresCampos[j]);
+                    }
+
+                    int possui_cod_estacao = 0;
+                    int chave_buscada;
+
+                    for (j = 0; j < m; j++) {
+                        if (strcmp(nomesCampos[j], "codEstacao") == 0) {
+                            possui_cod_estacao = 1;
+                            chave_buscada = atoi(valoresCampos[j]);
+                            break;
+                        }
+                    }
+
+                    fseek(index, 0, SEEK_SET);
+
+
+                    if(possui_cod_estacao){
+                        //retorna o rrn da busca em arvoreB
+                        int posicao_na_pagina=-1;
+                        int rrn_encontrado = busca_arvore(index, ci.noRaiz, chave_buscada, &posicao_na_pagina);
+                        
+                        //temos Um rrn e a chave existe dentro desse nó?
+                        if (rrn_encontrado != -1 && posicao_na_pagina != -1) {
+                            //então vamos ler o conteudo da pagina
+                            indice no_atual;
+                            fseek(index, calculo_byteoffset_indice(rrn_encontrado), SEEK_SET);
+                            ler_indice(index, &no_atual);
+
+                            long byte_dados = -1;
+                            if (posicao_na_pagina == 1) byte_dados = no_atual.Pr1;
+                            else if (posicao_na_pagina == 2) byte_dados = no_atual.Pr2;
+                            else if (posicao_na_pagina == 3) byte_dados = no_atual.Pr3;
+
+                            if (byte_dados != -1) {
+                                dados reg_dados;
+                                memset(&reg_dados, 0, sizeof(dados));
+                                
+                                fseek(bin, byte_dados, SEEK_SET);
+                                ler_regdados(bin, &reg_dados);
+                                
+                                if (reg_dados.removido != '1' && verificar_criterios(m, nomesCampos, valoresCampos, &reg_dados)) {
+                                    imprime_registro_dados(&reg_dados);
+                                } else {
+                                    printf("Registro inexistente.\n");
+                                }
+                            } else {
+                                printf("Registro inexistente.\n");
+                            }
+                        } else {
+                            // Se a árvore retornou -1, o código de estação simplesmente não existe
+                            printf("Registro inexistente.\n");
+                        }
+                    }else{
+                        buscar_registros(bin,tabela,m,nomesCampos,valoresCampos);
+                    }
+                    printf("\n");
+                }
+                fclose(index);
+                fclose(bin);
 
 
                 break;
