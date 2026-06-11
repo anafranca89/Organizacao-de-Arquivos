84227630abbcd566015c35827524830e8d53b5e6
bertholeo <leobbertho42@gmail.com>
2026-06-11 16:08:46 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -1,28 +1,24 @@
 #ifndef FUNCOES_H
 #define FUNCOES_H
 
+#define DEBUG 0
 #define MAX_LINHA_CSV 512
 #define TAM_REG 80
 #define TAM_CABECALHO 17
-#define TAM_TABELA 997  // número primo
 
-typedef struct ArquivoAberto {
-    char nome[100];
-    struct ArquivoAberto *prox;
-} ArquivoAberto;
+#include "estruturas.h"
+#define TAM_TABELA 997  
 
+// .h normal, veja .c para comentários mais detalhados e implementações
+void ler_par_campo_valor(char *campo, char *valor);
 
-typedef struct no_hash {
-    char nomeLinha[100];
-    int repeticoes;
-    struct no_hash *prox;
-} NoHash;
+int verificar_criterios(int m, char nomesCampos[][50],
+     char valoresCampos[][200], int codEstacao,
+      int tamNomeEstacao, char *nomeEstacao, int codLinha, int tamNomeLinha, char *nomeLinha, int codProxEstacao, int distProxEstacao, int codLinhaIntegra, int codEstIntegra);
 
-void inicializar_tabela(NoHash *tabela[]);
-void liberar_tabela(NoHash *tabela[]);
-void inserir_hash(NoHash *tabela[], char *nomeLinha, int tamNomeLinha);
-NoHash* buscar_hash(NoHash *tabela[], char *nomeLinha, int tamNomeLinha);
-
+void atualizar_campos_registro(int p, char nomesAtualiza[][50],
+     char valoresAtualiza[][200], int *codEstacao, int *tamNomeEstacao,
+      char *nomeEstacao, int *codLinha, int *tamNomeLinha, char *nomeLinha, int *codProxEstacao, int *distProxEstacao, int *codLinhaIntegra, int *codEstIntegra);
 
 int ler_registro_csv(FILE *csv,
                      int *codEstacao,
@@ -44,10 +40,26 @@ void mostrar_binario_sequencial(FILE *bin);
 void buscar_registros(FILE *bin, NoHash *tabela[], int m,
                                 char nomesCampos[][50],
                                 char valoresCampos[][200]);
-int arquivo_ja_processado(ArquivoAberto *lista, char *nome_bin);
-void adicionar_arquivo_processado(ArquivoAberto **lista, char *nome_bin);
-void liberar_lista_arquivos(ArquivoAberto *lista);
-void carregar_nomes_no_hash(FILE *bin, NoHash *tabela[]);
+
+void remover_registros_dinamico(FILE *bin,
+                                NoHash *tabela[],
+                                int m,
+                                char nomesCampos[][50],
+                                char valoresCampos[][200],
+                                int *topo, char ultimo);
+
+void inserir_registro_dinamico(FILE *bin, NoHash *tabela[],
+                               int codEstacao, int codLinha, int codProxEstacao, int distProxEstacao,
+                               int codLinhaIntegra, int codEstIntegra,
+                               int tamNomeEstacao, char *nomeEstacao,
+                               int tamNomeLinha, char *nomeLinha);
+
+void atualizar_registros_dinamico(FILE *bin, NoHash *tabela[],
+                                  int m, char nomesBusca[][50], char valoresBusca[][200],
+                                  int p, char nomesAtualiza[][50], char valoresAtualiza[][200], char status);
+
+void printar_registro_inteiro(int codEstacao, int tamNomeEstacao, char *nomeEstacao, int codLinha, int tamNomeLinha,
+     char *nomeLinha, int codProxEstacao, int distProxEstacao, int codLinhaIntegra, int codEstIntegra);                                  
+void ler_e_inserir_registro(FILE *bin, NoHash *tabela[]);              
 
 #endif
-
