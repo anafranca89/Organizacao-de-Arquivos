6af3ce8fa856057d059359e9b150fffcd2440913
anafranca89 <anafranca8933@gmail.com>
2026-06-16 11:23:06 -0300

    Hash totalmente comentado

--- hash.c
+++ hash.c
@@ -8,59 +8,15 @@
 
 
 
-// Formula para criação da chave hash 
-int hash_string(char *str, int tam, int primo) {
+int hash_string(char *str, int tam) {
     int soma = 0;
-
+    // Formula para criação da chave hash 
     for (int i = 0; i < tam; i++) {
         soma += (unsigned char) str[i];
     }
 
-    return soma % primo;
+    return soma % TAM_TABELA;
 }
-
-
-
-
-
-/*Estrutura escolhida para auxiliar na busca de string unicas foi tabela Hash.
-
-Se o tamnaho das strings é >0 insere na tabela hash.*/
-void carregar_nomes_no_hash(FILE *bin, NoHash *tabela[]) {
-    
-    if (bin == NULL) {
-        return;
-    }
-    cabecalho reg_cab;
-    dados reg_dados;
-    int rrn;
-
-
-    fseek(bin, 0, SEEK_SET);
-    ler_cabecalho(bin, &reg_cab);
-
-    for (rrn = 0; rrn < reg_cab.proxRRN; rrn++) {
-        reg_dados = cria_dados();
-        fseek(bin, calculo_byteoffset_dados(rrn), SEEK_SET);
-        ler_regdados(bin, &reg_dados);
-
-        if (reg_dados.removido == '1') {
-            continue;
-        }
-        if (reg_dados.tamNomeEstacao > 0) {
-            NoHash *h = buscar_hash(tabela, reg_dados.nomeEstacao, reg_dados.tamNomeEstacao);
-            if (h == NULL) {
-                inserir_hash(tabela, reg_dados.nomeEstacao, reg_dados.tamNomeEstacao);
-            } else {
-                h->repeticoes++; 
-            }
-        }
-    }
-    fseek(bin, 0, SEEK_SET);
-}
-
-
-
 
 
 
@@ -93,11 +49,57 @@ void liberar_tabela(NoHash *tabela[]) {
 
 
 
-void inserir_hash(NoHash *tabela[], char *nomeEstacao, int tamnomeEstacao) {
-    int pos = hash_string(nomeEstacao, tamnomeEstacao, TAM_TABELA);
-    NoHash *atual = tabela[pos];
 
+
+/*Estrutura escolhida para auxiliar na busca de string unicas foi tabela Hash.
+Se o tamnaho das strings é >0 insere na tabela hash.*/
+void carregar_nomes_no_hash(FILE *bin, NoHash *tabela[]) {
+     
+    cabecalho reg_cab;
+    dados reg_dados;
+    int rrn;
+
+    ler_cabecalho(bin, &reg_cab);
+    
+    for (rrn = 0; rrn < reg_cab.proxRRN; rrn++) {
+        reg_dados = cria_dados();
+        //fseek(bin, calculo_byteoffset_dados(rrn), SEEK_SET);
+        ler_regdados(bin, &reg_dados);
+
+
+        //é removido ?
+        if (reg_dados.removido == '1') {
+            //o ponteiro já está no final desse rrn logo, vai p/ proxima iteração
+            continue;
+        }    
+        if (reg_dados.tamNomeEstacao > 0) {
+            // a string existe no hash?
+            NoHash *h = buscar_hash(tabela, reg_dados.nomeEstacao, reg_dados.tamNomeEstacao);
+            if (h == NULL) {
+                inserir_hash(tabela, reg_dados.nomeEstacao, reg_dados.tamNomeEstacao);
+            } else {
+                // apenas aumenta as repetições
+                h->repeticoes++; 
+            }    
+        }    
+    }   
+    //atualiza o ponteiro do arquivo de dados p/ usar em outras funções. 
+    fseek(bin, 0, SEEK_SET);
+
+}    
+
+
+
+
+
+
+
+void inserir_hash(NoHash *tabela[], char *nomeEstacao, int tamnomeEstacao) {
+    int pos = hash_string(nomeEstacao, tamnomeEstacao);
+    NoHash *atual = tabela[pos];
+    //estamos no bucket correto da string 
     while (atual != NULL) {
+        //houve colisões? verifica se a  string armazenada é a msm
         if ((int)strlen(atual->nome) == tamnomeEstacao &&
             strncmp(atual->nome, nomeEstacao, tamnomeEstacao) == 0) {
             atual->repeticoes++;
@@ -106,6 +108,7 @@ void inserir_hash(NoHash *tabela[], char *nomeEstacao, int tamnomeEstacao) {
         atual = atual->prox;
     }
 
+    //a strin não existia - cria novo Nó
     NoHash *novo = (NoHash *) malloc(sizeof(NoHash));
     if (novo == NULL) return;
     memcpy(novo->nome, nomeEstacao, tamnomeEstacao);
@@ -118,35 +121,21 @@ void inserir_hash(NoHash *tabela[], char *nomeEstacao, int tamnomeEstacao) {
 
 
 
-NoHash* buscar_hash(NoHash *tabela[], char *nomeEstacao, int tamnomeEstacao) {
-    int pos = hash_string(nomeEstacao, tamnomeEstacao, TAM_TABELA);
-    NoHash *atual = tabela[pos];
-
-    while (atual != NULL) {
-        if ((int)strlen(atual->nome) == tamnomeEstacao &&
-            strncmp(atual->nome, nomeEstacao, tamnomeEstacao) == 0) {
-            return atual;
-        }
-        atual = atual->prox;
-    }
-
-    return NULL;
-}
 
 
 
-// caso for encontrado uma repetição, decrementa o hashing
 void decrementar_hash(NoHash *tabela[], char *nomeEstacao, int tamNomeEstacao) {
-    int pos = hash_string(nomeEstacao, tamNomeEstacao, TAM_TABELA);
+    int pos = hash_string(nomeEstacao, tamNomeEstacao);
     NoHash *atual = tabela[pos];
     NoHash *anterior = NULL;
-
+    
+    // caso for encontrado uma repetição, decrementa o hashing
     while (atual != NULL) {
         if ((int)strlen(atual->nome) == tamNomeEstacao &&
             strncmp(atual->nome, nomeEstacao, tamNomeEstacao) == 0) {
 
             atual->repeticoes--;
-
+            
             if (atual->repeticoes <= 0) {
                 if (anterior == NULL) {
                     tabela[pos] = atual->prox;
@@ -157,7 +146,7 @@ void decrementar_hash(NoHash *tabela[], char *nomeEstacao, int tamNomeEstacao) {
             }
             return;
         }
-
+        
         anterior = atual;
         atual = atual->prox;
     }
@@ -165,6 +154,20 @@ void decrementar_hash(NoHash *tabela[], char *nomeEstacao, int tamNomeEstacao) {
 
 
 
+NoHash* buscar_hash(NoHash *tabela[], char *nomeEstacao, int tamnomeEstacao) {
+    int pos = hash_string(nomeEstacao, tamnomeEstacao);
+    NoHash *atual = tabela[pos];
+    //retorna o ponteiro do nó da string encontrada
+    while (atual != NULL) {
+        if ((int)strlen(atual->nome) == tamnomeEstacao &&
+            strncmp(atual->nome, nomeEstacao, tamnomeEstacao) == 0) {
+            return atual;
+        }
+        atual = atual->prox;
+    }
+    //não encontrou a string
+    return NULL;
+}
 
 
 
--- hash.h
+++ hash.h
@@ -5,7 +5,10 @@
 #include "registros.h"
 
 
-/*Estrutura HASH para lógica de contar Estações únicas rapidamente
+/*---------------------------
+Estrutura HASH para lógica de contar Estações únicas rapidamente
+
+---------------------------
 */
 typedef struct no_hash {
     char nome[100];
@@ -14,16 +17,35 @@ typedef struct no_hash {
 } NoHash;
 
 
-
+/*---------------------------
+Construtor  e desalocação de memória da lista encadeada p/Hash
+---------------------------
+*/
+    /* fórmula p/ criar chave única do nome estação baseado no tamanho 
+    da string e a propria string.  */
+int hash_string(char *str, int tam);
+    //construtor da tabela - todos os ponteiros como NULL
 void inicializar_tabela(NoHash *tabela[]);
+    //free em cada nó da lista encadeada.
 void liberar_tabela(NoHash *tabela[]);
-int hash_string(char *str, int tam, int primo);
 
 
-void inserir_hash(NoHash *tabela[], char *nomeLinha, int tamNomeLinha);
+/*---------------------------
+Funções Base Hash:
+    ->inserir: cria um novo nó no bucket[posicao dado por hash_string]
+                e aumenta o numero de repetições daquela string
+    -> Decrementa: remove a string do bucket[posicao] ou apenas diminue o numero de repetições
+    -> Busca: Dado uma string, procura no bucket[posicao], retorna o ponteiro do no, ou NULL se não existe
+    -> carregar_nomes: dado um arquivo binário de registro de dados, usa insere p/ armazenar as Estações únicas.
+*/
+        /*  Ponteiro do fseek no COMEÇO do arquivo antes de chamar a função!
+        precisa dos ponteiros arqivo binario de dados ABERTO em leitura e tabela HASH inicializada*/
 void carregar_nomes_no_hash(FILE *bin, NoHash *tabela[]);
+    //precisa da tabela HASH inicializada, e o tamanho da string e a propra string a inserir
+void inserir_hash(NoHash *tabela[], char *nomeLinha, int tamNomeLinha);
+    //precisa da tabela HASH inicializada, e o tamanho da string e a propra string a remover
 void decrementar_hash(NoHash *tabela[], char *nomeEstacao, int tamNomeEstacao);
-
+    //precisa da tabela HASH inicializada, e o tamanho da string e a propra string a buscar
 NoHash* buscar_hash(NoHash *tabela[], char *nomeLinha, int tamNomeLinha);
 
 
